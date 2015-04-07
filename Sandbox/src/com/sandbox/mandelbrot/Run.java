f65eace829dcbe1f7af29da2a230573c7ab46274
Mitchell Skaggs <skaggsm333@gmail.com>
2015-04-07 16:29:00 -0500

    Added colors and zoom to the Mandelbrot set visualization.

--- Sandbox/src/com/sandbox/mandelbrot/Run.java
+++ Sandbox/src/com/sandbox/mandelbrot/Run.java
@@ -3,22 +3,27 @@ package com.sandbox.mandelbrot;
 import java.awt.Color;
 import java.awt.Graphics;
 
-import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import org.apache.commons.math3.complex.Complex;
 
 public class Run
 {
+
 	public static void main(String[] args)
 	{
-		JComponent comp = new JComponent()
+		JPanel comp = new JPanel()
 		{
 			private static final long	serialVersionUID	= 1L;
-			public static final double	scale				= 200;
+			public int					renderResolution	= 8;
+			public double				scale				= 200;
+			public double				xTarget				= 86;
+			public double				yTarget				= 75;
+			public double				zoomMultiplier		= 1.5;
 
 			@Override
-			public void paint(Graphics g)
+			public void paintComponent(Graphics g)
 			{
 				long startTime = System.nanoTime();
 				double normalizedValue = 0;
@@ -26,42 +31,40 @@ public class Run
 				final int height = this.getHeight();
 				double max = maxIteration;
 
-				Complex c = new Complex(0, 0);
-				for (int x = 0; x < width; x++)
+				Complex c = null;
+				for (int x = 0; x < width; x += renderResolution)
 				{
-					for (int y = 0; y < height; y++)
+					for (int y = 0; y < height; y += renderResolution)
 					{
-						c = new Complex((x - (5 * width / 8)) / scale, (y - (height / 2)) / scale);
+						c = new Complex((x - (width / 2) + xTarget) / scale, (y - (height / 2) + yTarget) / scale);
 						normalizedValue = 1 - (mandelbrotDivergeRate(c) / max);
-						try
-						{
-							g.setColor(new Color((int) (normalizedValue * 255), (int) (normalizedValue * 255), (int) (normalizedValue * 255)));
-						}
-						catch (IllegalArgumentException e)
-						{
-							//System.out.println(mandelbrotDivergeRate(c) + " / " + max);
-							g.setColor(Color.BLACK);
-						}
-						g.drawLine(x, y, x, y);
+						g.setColor(Run.getRainbow(normalizedValue * 5));
+						g.fillRect(x, y, renderResolution, renderResolution);
 					}
 				}
-				System.out.println(String.format("Frame took %.3f seconds to render.", (System.nanoTime() - startTime) / 1000000000d));
+				// renderResolution--;
+				// if (renderResolution < 1) renderResolution = 1;
+				scale *= zoomMultiplier;
+				xTarget *= zoomMultiplier;
+				yTarget *= zoomMultiplier;
+				System.out.println(String.format("Frame took %.4f seconds to render.", (System.nanoTime() - startTime) / 1000000000d));
+				repaint();
 			}
 		};
 
 		JFrame frame = new JFrame();
 		frame.add(comp);
-		frame.setSize(700, 600);
+		frame.setSize(400, 300);
 		frame.setBackground(Color.WHITE);
 		frame.setVisible(true);
 	}
 
-	public static final double	bound	= 2;
-	public static final int maxIteration = 50;
+	public static final double	bound			= 2;
+	public static final int		maxIteration	= 25;
 
 	public static int mandelbrotDivergeRate(Complex c)
 	{
-		for (int i = 1; i <= maxIteration; i++)
+		for (int i = 0; i < maxIteration; i++)
 		{
 			if (getMandelbrotIteration(c, i).abs() > bound) return i;
 		}
@@ -78,5 +81,16 @@ public class Run
 		{
 			return getMandelbrotIteration(c, n - 1).pow(2).add(c);
 		}
+	}
+
+	public static Color getRainbow(double i)
+	{
+		int r = 128 + (int) (127 * Math.sin(i));
+		int g = 128 + (int) (127 * Math.sin(i + (2 * Math.PI / 3)));
+		int b = 128 + (int) (127 * Math.sin(i + (4 * Math.PI / 3)));
+		if (i > 0)
+			return new Color(r, g, b);
+		else
+			return Run.getRainbow(1);
 	}
 }
